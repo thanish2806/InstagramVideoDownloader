0fea39e12beb525bbede1b80e257e5ce2e07b119
Thanish <thanish059@gmail.com>
2025-11-12 16:04:43 +0530

    `Added meta description to index.html, CSS styles to App.css and HomePage.css, and updated HomePage.jsx with new layout and components`

---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+function Navbar() {
+  return (
+    <nav className="navbar-section">
+      <h2 className="navbar-title">I Down</h2>
+    </nav>
+  );
+}
+export default Navbar;
--- src/HomePage.jsx
+++ src/HomePage.jsx
@@ -1,10 +1,23 @@
-import React  from "react";
-
-
+import React from "react";
+import "./HomePage.css";
+import Navbar from "./Components/Navbar";
 function HomePage() {
   return (
-    <div>
-      <h1>Welcome to the Home Page</h1>
+    <div className="homepage-section">
+      <Navbar />
+      <div className="homepage-container">
+        <div className="bg-banner">
+          <h1 className="title-01">Instagram Video Downloader</h1>
+          <div className="input-section">
+            <input
+              type="text"
+              className="input-url"
+              placeholder="Paste Instagram video link here"
+            />
+            <button className="download-btn">Download</button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
